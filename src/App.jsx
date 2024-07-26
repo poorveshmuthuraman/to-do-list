b64c047421f69f3c49b6fee56a04cca371beae51
poorveshmuthuraman <m.poorvesh@gmail.com>
2024-07-26 11:47:16 +0530

    add local storage and remove hardcoded values in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,27 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import TodoInput from './components/TodoInput'
 import TodoList from './components/TodoList'
 
 function App() {
 
-  const [todos, setTodos] = useState([
-    'go to the gym',
-    'drink coffee',
-    'do meditation'
-  ]);
+  const [todos, setTodos] = useState([]);
 
   const [currentTodo, setCurrentTodo] = useState('')
 
+  function persistData(newList) {
+    localStorage.setItem('todos', JSON.stringify({todos: newList}))
+  }
+
   function handleNewTodo(newTodo){ //going to pass this function in through props
     const newTodos = [...todos, newTodo];
+    persistData(newTodos)
     setTodos(newTodos);
   }
 
   function handleDeleteTodo(index) {
     const newTodos = todos.filter((_, todoIndex) => {return index != todoIndex })
+    persistData(newTodos)
     setTodos(newTodos)
   }
 
@@ -28,6 +30,20 @@ function App() {
     setCurrentTodo(valueToBeEdited)
     handleDeleteTodo(index)
   }
+
+  useEffect(() => {
+    if(!localStorage){
+      return
+    }
+
+    let localTodos = localStorage.getItem("todos")
+    if(!localTodos) {
+      return
+    }
+
+    localTodos = JSON.parse(localTodos).todos
+    setTodos(localTodos)
+  }, [])
 
   return (
     <>
